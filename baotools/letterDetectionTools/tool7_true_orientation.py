ec206237686fc928d06358d5a3b4b1db82a82c22
LiangYang666 <mail.liangyang@gmail.com>
2021-09-06 08:52:00 +0800

    实现对真类图片的翻转，按照exif信息翻转保存，保存的新图片不含exif信息，避免不同读取图片模块读入结果不同

---
+++ baotools/letterDetectionTools/tool7_true_orientation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :tool7_true_orientation.py
+# @Time      :2021/9/5 下午3:30
+# @Author    :Yangliang
+ 
+ 
+if __name__ == "__main__":
+    run_code = 0
--- script_detect_letter.sh
+++
@@ -1,9 +0,0 @@
-python \
-detect_letter.py \
---brand Chanel \
---part sign \
---weights /media/D_4TB/YL_4TB/BaoDetection/data/Chanel/LetterDetection/data/yolov5_rundata/train20/weights/ckp_aug_all_6c_epoch1760.pt \
---source /media/D_4TB/YL_4TB/BaoDetection/data/Chanel/LetterDetection/data/sign/sign_2_need_已鉴定标记 \
---batch-size 10 \
---iou-thres 0.2 \
---conf-thres 0.5

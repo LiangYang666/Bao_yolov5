df048f022a7427ea93f13922d5f88155d0ad6d9b
LiangYang666 <mail.liangyang@gmail.com>
2021-09-23 20:51:36 +0800

    将获取字母交并和裁剪字母两个步骤放入到一个py文件中

--- baotools/letterDetectionTools/tool5_cut_intersect_letter_imgxml.py
+++ baotools/letterDetectionTools/tool5_cut_intersect_letter_imgxml.py
@@ -4,12 +4,16 @@
 # @Time      :2021/9/2 下午3:53
 # @Author    :Yangliang
 
-# 获取正确检测到字母框 且xml标签含有对对应字母（如C非正圆）的标记框的图片及xml 将其复制到指定文件夹内做进一步处理
+# 获取正确检测到字母框 且xml标签含有对对应字母（如C非正圆）的标记框的图片及xml 将其复制到指定文件夹内做进一步处理 并裁剪有问题的字母图到新文件夹
 import json
 import os
+import re
 import shutil
 import warnings
 import xml.etree.ElementTree as ET
+
+import cv2
+import matplotlib.pyplot as plt
 
 
 def calculate_iou(bbox1, bbox2):
@@ -36,8 +40,9 @@ def check_if_intersect(xml_path, xyxy):
         y1 = int(bandbox.find('ymin').text)
         x2 = int(bandbox.find('xmax').text)
         y2 = int(bandbox.find('ymax').text)
-        if calculate_iou(xyxy, [x1,y1,x2,y2])>0.2:
-            return True
+        if calculate_iou(xyxy, [x1, y1, x2, y2]) > 0.2:
+            print(xml_path, calculate_iou(xyxy, [x1, y1, x2, y2]))
+            return name
     return False
 
 
@@ -77,21 +82,34 @@ def get_json_info(detected_img_dir):
     return detected_info
 
 
-def copy_intersect_img_xml(detected_info, detected_img_name, category_id, src_img_dir, src_xml_dir, dst_img_dir, dst_xml_dir):
+def copy_intersect_img_xml_cutimg(detected_info, detected_img_name, category_id, src_img_dir, src_xml_dir, dst_img_dir, dst_xml_dir, dst_cut_img_dir):
     # detected_img_name = 'Chanel-2-doing_710803_金属件_sign.jpg'
     xml_name = detected_img_name.rsplit('.', 1)[0]+'.xml'
     src_xml_path = os.path.join(src_xml_dir, xml_name)
     get_total = 0
     if os.path.exists(src_xml_path):
         detected_xyxys = get_detected_xyxy_of_category(detected_info, detected_img_name, category_id)
-        for detected_xyxy in detected_xyxys:
-            if check_if_intersect(src_xml_path, detected_xyxy):
+        for i, detected_xyxy in enumerate(detected_xyxys):
+            rs = check_if_intersect(src_xml_path, detected_xyxy)
+            if isinstance(rs, str):
+                # 存储完整图片
                 src = os.path.join(src_img_dir, detected_img_name)
                 dst = os.path.join(dst_img_dir, detected_img_name)
                 shutil.copy(src, dst)
+                img = cv2.imread(src)
                 src = os.path.join(src_xml_dir, xml_name)
                 dst = os.path.join(dst_xml_dir, xml_name)
                 shutil.copy(src, dst)
+                # 存储裁剪图片
+                x1, y1, x2, y2 = detected_xyxy
+                img_cut = img[y1:y2 + 1, x1:x2 + 1]
+                img_save_path = os.path.join(dst_cut_img_dir, img_name)
+                temp = img_save_path.rsplit('.', 1)
+                rstr = r"[\/\\\:\*\?\"\<\>\|\.\s]"  # '/ \ : * ? " < > | .和空格'
+                rs = re.sub(rstr, "", rs)
+                img_save_path = f'{temp[0]}_{rs}_{i}.{temp[1]}'
+                cv2.imwrite(img_save_path, img_cut)
+
                 get_total += 1
     return get_total
 
@@ -100,6 +118,19 @@ def get_category_names(categories_file):
         lines = f.readlines()
         categories = [line.strip() for line in lines if len(line.strip()) >= 1]
     return categories
+
+
+def check_and_build(*dirs):
+    for dir in dirs:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+
+
+def remove_and_rebuild(*dirs):
+    for dir in dirs:
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
+        os.mkdir(dir)
 
 
 
@@ -120,18 +151,17 @@ if __name__ == "__main__":
             warnings.warn(f'{img_name} 可能未准确识别到')
     print('all_categories: ', categories)
     dst_p_dir = f'/media/D_4TB/YL_4TB/BaoDetection/data/{brand}/LetterDetection/data/{part}/{part}_2_handle/2与1重复出现且对某字母打了标签的'
-    if not os.path.exists(dst_p_dir):
-        os.mkdir(dst_p_dir)
+    dst_cut_p_dir = f'/media/D_4TB/YL_4TB/BaoDetection/data/{brand}/LetterDetection/data/{part}/{part}_2_handle/3裁剪后鉴定有问题的字母图片'
+    check_and_build(dst_p_dir, dst_cut_p_dir)
 
     for category in categories:
         dst_img_dir = dst_xml_dir = os.path.join(dst_p_dir, category)
-        if os.path.exists(dst_img_dir):
-            shutil.rmtree(dst_img_dir)
-        os.mkdir(dst_img_dir)
+        dst_cut_img_dir = os.path.join(dst_cut_p_dir, category)
+        remove_and_rebuild(dst_img_dir, dst_cut_img_dir)
         category_id = categories.index(category)
         total = 0
         for img_name in img_names:
-            total += copy_intersect_img_xml(detected_info, img_name, category_id, src_img_dir, src_xml_dir, dst_img_dir, dst_xml_dir)
+            total += copy_intersect_img_xml_cutimg(detected_info, img_name, category_id, src_img_dir, src_xml_dir, dst_img_dir, dst_xml_dir, dst_cut_img_dir)
         copied_img_names = os.listdir(dst_img_dir)
 
         detected_selected_info = {}
@@ -141,6 +171,15 @@ if __name__ == "__main__":
         print(f'{category}({category_id}): total {total}')
         with open(os.path.join(dst_img_dir, f'{category}_intersect_dicts.json'), 'w') as f:
             json.dump(detected_selected_info, f, indent=2)
+
+        # for name in detected_info.keys():
+        #     if '740100' in name:
+        #         img_orgin = cv2.imread(os.path.join(src_img_dir, name))
+        #         for bbox in detected_info[name]['3']:
+        #             x1, y1, x2, y2 = bbox
+        #             cv2.rectangle(img_orgin, (x1, y1), (x2, y2), (255, 0, 0), thickness=2)
+        #         save_path = os.path.join(os.path.dirname(dst_p_dir), name)
+        #         cv2.imwrite(save_path, img_orgin)
 
 
 
